15ba43df8943cc5fe6865297573f9a64f2e00804
Sylvain Barré <sbarre@alinto.eu>
2025-03-17 14:56:10 +0100

    feat: add tests and update i18n utilities
    - Added new test files:
      - src/lib/__tests__/utils.test.ts
      - src/lib/i18n/__tests__/utils.test.ts
    - Modified i18n utility files:
      - src/lib/i18n/request.ts
      - src/lib/i18n/utils.ts

--- src/lib/i18n/request.ts
+++ src/lib/i18n/request.ts
@@ -1,40 +1,51 @@
-import { getRequestConfig } from "next-intl/server";
-import { routing } from "@/middleware";
-import fs from "fs";
-import deepmerge from "deepmerge";
-import { transformJson } from "@/lib/i18n/utils";
+import { transformJson } from '@/lib/i18n/utils'
+import { routing } from '@/middleware'
+import deepmerge from 'deepmerge'
+import fs from 'fs'
+import { getRequestConfig } from 'next-intl/server'
+
+export const createDefaultMessages = async () => {
+  const defaultFileList = fs.readdirSync(
+    `src/messages/${routing.defaultLocale}`
+  )
+  const defaultMessages = {}
+  for (const file of defaultFileList) {
+    const msgs = await import(`@/messages/${routing.defaultLocale}/${file}`)
+    const newMessages = transformJson({
+      ...msgs.default,
+    })
+    Object.assign(defaultMessages, newMessages)
+  }
+  return defaultMessages
+}
+
+export const createMessages = async (locale: string) => {
+  const fileList = fs.readdirSync(`src/messages/${locale}`)
+  const messages = {}
+  for (const file of fileList) {
+    const msgs = await import(`@/messages/${locale}/${file}`)
+    const newMessages = transformJson({
+      ...msgs.default,
+    })
+    Object.assign(messages, newMessages)
+  }
+  return messages
+}
 
 export default getRequestConfig(async ({ requestLocale }) => {
   // This typically corresponds to the `[locale]` segment
-  let locale = await requestLocale;
+  let locale = await requestLocale
 
   // Ensure that a valid locale is used
   if (!locale || !routing.locales.includes(locale as any)) {
-    locale = routing.defaultLocale;
+    locale = routing.defaultLocale
   }
 
-  const defaultFileList = fs.readdirSync(
-    `src/messages/${routing.defaultLocale}`
-  );
-  const defaultMessages = {};
-  for (const file of defaultFileList) {
-    const newMessages = transformJson({
-      ...(await import(`@/messages/${routing.defaultLocale}/${file}`)).default,
-    });
-    Object.assign(defaultMessages, newMessages);
-  }
-  const messages = {};
-  if (locale === routing.defaultLocale) {
-    const fileList = fs.readdirSync(`src/messages/${locale}`);
-    for (const file of fileList) {
-      const newMessages = transformJson({
-        ...(await import(`@/messages/${locale}/${file}`)).default,
-      });
-      Object.assign(messages, newMessages);
-    }
-  }
+  const defaultMessages = await createDefaultMessages()
+  const messages = await createMessages(locale)
+
   return {
     locale,
     messages: deepmerge(defaultMessages, messages),
-  };
-});
+  }
+})
