8a6224bc56e95235b747e902486c505e1acc4d90
Sylvain Barré <sbarre@alinto.eu>
2025-10-20 16:35:40 +0200

    fix the overflow in the app

--- src/app/[locale]/(loggedin)/layout.tsx
+++ src/app/[locale]/(loggedin)/layout.tsx
@@ -36,7 +36,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <AppSidebar />
         <SidebarInset>
           <AppHeader />
-          <div className="h-[calc(100vh-48px)] gap-4 overflow-hidden border-1">
+          <div className="h-[calc(100vh-48px)] gap-4 overflow-auto border-1">
             {children}
           </div>
         </SidebarInset>
