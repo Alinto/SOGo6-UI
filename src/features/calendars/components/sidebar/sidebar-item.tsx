1bfd58d8f65e929fc9c1c5f9695613b819e6f674
Thomas Monmousseau <thomas.monmousseau@gmail.com>
2026-01-22 17:32:46 +0100

    feat(calendars): fix form create

--- src/features/calendars/components/sidebar/sidebar-item.tsx
+++ src/features/calendars/components/sidebar/sidebar-item.tsx
@@ -147,7 +147,7 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
               </DialogTrigger>
             </DropdownMenuContent>
           </DropdownMenu>
-          <DialogContent>
+          <DialogContent className="max-w-[calc(100vw-2rem)] sm:max-w-2xl">
             {type === 'edit' && (
               <EditForm
                 id={id}
