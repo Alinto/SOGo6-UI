ca3f9489972285cabe4280deeb40b7d7b08718eb
cpottier <cpottier@alinto.eu>
2026-07-16 16:10:39 +0200

    fix: adjust scroll for email content

--- src/app/[locale]/(loggedin)/u/[account]/[folder]/[mail_id]/layout.tsx
+++ src/app/[locale]/(loggedin)/u/[account]/[folder]/[mail_id]/layout.tsx
@@ -3,5 +3,5 @@
 import React from 'react'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="w-full">{children}</div>
+  return <div className="h-full w-full overflow-y-auto">{children}</div>
 }
